c1893938ad63640fbb79a49658d699172fd91d11
Jakub Rada <jakub.rd@seznam.cz>
2026-01-07 21:06:32 +0100

    new: divide each page into new file for better transaparency

---
+++ js/direction.js
@@ -0,0 +1,4 @@
+document.getElementById('directionBox').innerHTML = `
+    <button class="direction-btn long" onclick="selectDirection('long')">LONG</button>
+    <button class="direction-btn short" onclick="selectDirection('short')">SHORT</button>
+`;
